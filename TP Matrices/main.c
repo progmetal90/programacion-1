32b87ee94c62000fb86553a1f5f14647c0c078cc
peroquenariz <el.ace100@gmail.com>
2024-04-18 21:24:29 -0300

    TP Matrices terminado

--- TP Matrices/main.c
+++ TP Matrices/main.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#include <math.h>
+#include <conio.h>
 
 int menu();
 void imprimirEspacios(int cantidad);
@@ -15,8 +17,21 @@ int cargarArregloStrings(int cantidadCaracteres, char arregloStrings[][cantidadC
 void mostrarArregloStrings(int filas, int longitudPalabra, char arregloStrings[][longitudPalabra]);
 int cargarArregloStringsRandom(int cantidadCaracteres, char arregloStrings[][cantidadCaracteres], int validos, int cantidadStringsTotal, int cantidadRandom, char palabrasRandom[][cantidadRandom]);
 void ordenarPorSeleccion(int cantCaracteres, char arregloStrings[][cantCaracteres], int validos, int cantStrings);
+void mostrarVotos(int filas, int columnas, int matriz [filas][columnas]);
+int obtenerCantidadDigitos(int numero);
+void mostrarArregloEnteros(int arreglo[], int validos);
+void mostrarGanador(float porcentajes[], int validos);
+char candidato(int indice);
+void calcularVotos(int distritos, int candidatos, int votos[distritos][candidatos]);
+int obtenerCandidatoMasVotado(int candidatos, int votos[]);
+int existeStringEnArreglo(int validos, int cantCaracteres, char arreglo[][cantCaracteres], char string []);
+int busquedaBinaria(int cantCaracteres, char arregloStrings[][cantCaracteres], int validos, char buscador[]);
+
 
 const int cantidadRandom = 10;
+
+const int candidatos = 4;
+const int distritos = 5;
 
 int main()
 {
@@ -42,7 +57,7 @@ int main()
     int matriz [filas][columnas];
     int matrizManual [filas][columnas];
 
-    int cantStrings = 3;
+    int cantStrings = 7;
     int cantCaracteres = 8; // NO OLVIDARSE DEL NULL!
     char arregloStrings[cantStrings][cantCaracteres];
     int validosStrings = 0;
@@ -52,12 +67,21 @@ int main()
     int elementoABuscar = 6;
     int existe;
 
+    int votos[4][5] = {
+        194, 48, 206, 45,
+        180, 20, 320, 16,
+        221, 90, 140, 20,
+        432, 51, 821, 14,
+        820, 61, 940, 18
+    };
+
     cargarMatriz(filas, columnas, matriz);
     printf("Matriz cargada con numeros aleatorios: \n\n");
     mostrarMatriz(filas, columnas, matriz);
     printf("\n");
 
     int opcion = menu();
+    system("cls");
 
     switch(opcion){
         case 1:
@@ -105,6 +129,14 @@ int main()
             }
             printf("existe en el arreglo.");
             break;
+        case 10:
+            validosStrings = cargarArregloStringsRandom(cantCaracteres, arregloStrings, validosStrings, cantStrings, cantidadRandom, palabrasRandom);
+            ordenarPorSeleccion(cantCaracteres, arregloStrings, validosStrings, cantStrings);
+            mostrarArregloStrings(validosStrings, cantCaracteres, arregloStrings);
+
+            existe = busquedaBinaria(cantCaracteres, arregloStrings, validosStrings, "account");
+            printf("%d", existe);
+            break;
         case 11:
             validosStrings = cargarArregloStringsRandom(cantCaracteres, arregloStrings, validosStrings, cantStrings, cantidadRandom, palabrasRandom);
             mostrarArregloStrings(validosStrings, cantCaracteres, arregloStrings);
@@ -112,9 +144,178 @@ int main()
             ordenarPorSeleccion(cantCaracteres, arregloStrings, validosStrings, cantStrings);
             mostrarArregloStrings(validosStrings, cantCaracteres, arregloStrings);
             break;
+        case 12:
+            printf("Ejercicio no resuelto aun!");
+            break;
+        case 13:
+            printf("Ejercicio no resuelto aun!");
+            break;
+        case 14:
+            mostrarVotos(distritos, candidatos, votos);
+            calcularVotos(distritos, candidatos, votos);
+
+            break;
         }
 
     return 0;
+}
+
+int busquedaBinaria(int cantCaracteres, char arregloStrings[][cantCaracteres], int validos, char buscador[]){
+    int encontrado = 0; // 0: buscando, 1: encontrado, 2: no encontrado
+    int iMenor = 0;
+    int iMayor = validos-1;
+    int i = iMayor / 2;
+
+    while(encontrado == 0){
+        // Partir desde la mitad del rango disponible.
+        // Comprobar si es igual, mayor o menor.
+        int comparacion = strcmp(buscador, arregloStrings[i]);
+
+        if(comparacion == 0){
+            encontrado = 1;
+        }
+        else if(comparacion < 0){
+            // Nuestra palabra es menor a la comparada.
+            iMayor = i;
+            i = iMayor / 2;
+        }
+        else if(comparacion > 0){
+            // Nuestra palabra es mayor a la comparada.
+            iMenor = i;
+            i = (iMayor - iMenor + 1) / 2 + iMenor;
+        }
+
+        if(iMayor - iMenor == 0 && encontrado == 0){
+            // No hay mas nada que buscar
+            encontrado = 2;
+        }
+    }
+
+    if(encontrado == 2){
+        i = -1;
+    }
+    return i;
+}
+
+void calcularVotos(int distritos, int candidatos, int votos[distritos][candidatos]){
+    int totalVotos = 0;
+    int candidatoMasVotado;
+    int votosPorCandidato[] = {0, 0, 0, 0};
+    float porcentajeVotosPorCandidato[] = {0.0, 0.0, 0.0, 0.0};
+
+    for(int i = 0; i < distritos; i++){
+        for(int j = 0; j < candidatos; j++){
+            int votosCandidato = votos[i][j];
+            totalVotos += votosCandidato;
+            votosPorCandidato[j] += votosCandidato;
+        }
+    }
+
+    for(int i = 0; i < candidatos; i++){
+        porcentajeVotosPorCandidato[i] = (float)votosPorCandidato[i] * 100 / totalVotos;
+    }
+
+    printf("Total de votos emitidos: %d.\n", totalVotos);
+    printf("Votos por candidato: ");
+    mostrarArregloEnteros(votosPorCandidato, candidatos);
+
+    candidatoMasVotado = obtenerCandidatoMasVotado(candidatos, votosPorCandidato);
+
+    printf("El candidato mas votado fue el candidato %c.\n\n", 65+candidatoMasVotado);
+
+    mostrarGanador(porcentajeVotosPorCandidato, candidatos);
+
+    printf("\n");
+}
+
+void mostrarGanador(float porcentajes[], int validos){
+    int mayor = 0;
+    int segundo = 0;
+
+    for(int i = 0; i < validos && porcentajes[mayor] < 50; i++){
+        if(porcentajes[i] > porcentajes[mayor]){
+            mayor = i;
+        }
+    }
+
+    if(porcentajes[mayor] > 50){
+        printf("El candidato %c es el ganador con el %.2f%% de los votos.", candidato(mayor), porcentajes[mayor]);
+    }
+    else{
+        if(segundo == 0){
+            segundo++;
+        }
+
+        for(int i = 0; i < candidatos; i++){
+            if(i != mayor){
+                if(porcentajes[i] > porcentajes[segundo]){
+                    segundo = i;
+                }
+            }
+        }
+        printf("Los candidatos %c (%.2f%%) y %c (%.2f%%) competiran en ballotage!\n", candidato(mayor), porcentajes[mayor], candidato(segundo), porcentajes[segundo]);
+    }
+}
+
+char candidato(int indice){
+    return indice+65;
+}
+
+int obtenerCandidatoMasVotado(int candidatos, int votos[]){
+    int masVotado = 0;
+
+    for(int i = 1; i < candidatos; i++){
+        if(votos[i] > votos[masVotado]){
+            masVotado = i;
+        }
+    }
+
+    return masVotado;
+}
+
+void mostrarVotos(int filas, int columnas, int matriz [filas][columnas]){
+    int ancho = 12;
+    printf("DISTRITO    | CANDIDATO A | CANDIDATO B | CANDIDATO C | CANDIDATO D |\n");
+    for(int i = 0; i < filas; i++){
+        printf("%d", i+1);
+        imprimirEspacios(ancho-1);
+        for(int j = 0; j < columnas; j++){
+            int valor = matriz[i][j];
+            int padding = ancho - obtenerCantidadDigitos(valor);
+            printf("| %d", valor);
+            imprimirEspacios(padding);
+        }
+        printf("|\n");
+    }
+}
+
+void mostrarArregloEnteros(int arreglo[], int validos){
+    for(int i = 0; i < validos; i++){
+        printf("%d ", arreglo[i]);
+    }
+    printf("\n");
+}
+
+int obtenerCantidadDigitos(int numero){
+    int digitos = 0;
+
+    if(numero < 0){
+        digitos++;
+    }
+
+    numero = abs(numero);
+
+    if(numero == 0){
+        digitos = 1;
+    }
+    else{
+        while(numero != 0){
+        numero /= 10;
+        digitos++;
+        }
+    }
+
+    return digitos;
 }
 
 void ordenarPorSeleccion(int cantCaracteres, char arregloStrings[][cantCaracteres], int validos, int cantStrings){
@@ -184,6 +385,8 @@ int cargarArregloStrings(int cantidadCaracteres, char arregloStrings[][cantidadC
         option = getch();
         system("cls");
     }
+
+    return i;
 }
 
 void mostrarArregloStrings(int filas, int longitudPalabra, char arregloStrings[][longitudPalabra]){
